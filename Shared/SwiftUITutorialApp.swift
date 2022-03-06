227b42853b87e2a08a5a19b86bc64b1e259b03c6
dishi07 <0br3874w24.3c5z@ezweb.ne.jp>
2022-03-07 03:48:45 +0900

    add project

---
+++ Shared/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  Shared
+//
+//  Created by 石田大智 on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
