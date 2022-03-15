90368e0df160c93628ba7f138c1ae3a9ca31b1e1
dishi07 <0br3874w24.3c5z@ezweb.ne.jp>
2022-03-16 08:53:48 +0900

    fixed filter landmark row by favorite

--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -2,9 +2,17 @@ import SwiftUI
 import MapKit
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -17,9 +25,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-        }
+        LandmarkList()
     }
 }
