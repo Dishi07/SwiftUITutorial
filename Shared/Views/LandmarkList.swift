d42d112bc23eed89241610b2f921ba5ea73bf2d1
dishi07 <0br3874w24.3c5z@ezweb.ne.jp>
2022-03-16 08:57:53 +0900

    add toggle button

--- Shared/Views/LandmarkList.swift
+++ Shared/Views/LandmarkList.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import MapKit
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
 
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
@@ -12,13 +12,18 @@ struct LandmarkList: View {
 
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
-            }.navigationTitle("Landmarks")
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }.navigationTitle("Landmarks")
+            }
         }
     }
 }
