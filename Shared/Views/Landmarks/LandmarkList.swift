0701a9b28b292b4e4a1afaf99b582f4918a0ed21
dishi07 <0br3874w24.3c5z@ezweb.ne.jp>
2022-03-17 10:49:46 +0900

    create a favorite button for each landmark

--- Shared/Views/Landmarks/LandmarkList.swift
+++ Shared/Views/Landmarks/LandmarkList.swift
@@ -32,5 +32,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }
