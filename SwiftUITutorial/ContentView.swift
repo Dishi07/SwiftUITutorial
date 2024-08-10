4327fb7a224d5d4bc57dc6c198fae4384c7f135a
Dishi07 <dishi30000@gmail.com>
2024-08-10 14:55:15 +0900

    first commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by 石田大智 on 2024/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
